3f8003d9e737dbbfe1b6d1ebdaceadfd52f3bc84
silver-fox-99 <silverfox99m@gmail.com>
2025-04-24 19:38:55 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Включение CORS
+  app.enableCors({
+    origin: '*', // Разрешить все источники (замените * на конкретный домен для безопасности)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешённые методы
+    credentials: true, // Разрешить передачу cookies, если нужно
+  });
+
+  // Запуск приложения на указанном порту
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
